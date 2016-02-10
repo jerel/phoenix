1201daa88adaca4dcc3cadd6ee016df799d1a4dc
Jerel Unruh <mail@unruhdesigns.com>
2016-02-09 20:42:01 -0600

    Fixed InvalidStateError in IE11 when using LongPoll transport

--- web/static/js/phoenix.js
+++ web/static/js/phoenix.js
@@ -702,8 +702,8 @@ export class Ajax {
   }
 
   static xdomainRequest(req, method, endPoint, body, timeout, ontimeout, callback){
-    req.timeout = timeout
     req.open(method, endPoint)
+    req.timeout = timeout
     req.onload = () => {
       let response = this.parseJSON(req.responseText)
       callback && callback(response)
@@ -717,8 +717,8 @@ export class Ajax {
   }
 
   static xhrRequest(req, method, endPoint, accept, body, timeout, ontimeout, callback){
-    req.timeout = timeout
     req.open(method, endPoint, true)
+    req.timeout = timeout
     req.setRequestHeader("Content-Type", accept)
     req.onerror = () => { callback && callback(null) }
     req.onreadystatechange = () => {
